a818fc5cccf59f7aee0f8e199362fb15734051f0
razpopa <razvan.popa82@gmail.com>
2015-12-03 16:53:31 +0200

    Refs #29102; Solved PID1 issue, added version tag

--- src/run-virtuoso.sh
+++ src/run-virtuoso.sh
@@ -5,4 +5,18 @@ VIRT_INI_TPL='/tmp/virtuoso.ini.j2'
 
 j2 "$VIRT_INI_TPL" > $CONFIG_FILE
 
-/bin/bash /startup.sh
+#/bin/bash /startup.sh
+cd /var/lib/virtuoso/db
+
+mkdir -p dumps
+
+mv /virtuoso.ini . 2>/dev/null
+
+if [ ! -f "/.dba_pwd_set" ];
+then
+  virtuoso-t +wait && isql-v -U dba -P dba < /dump_nquads_procedure.sql && echo  "user_set_password('dba', '$DBA_PASSWORD');" | isql-v -U dba -P dba
+  kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
+  touch /.dba_pwd_set
+fi
+
+exec virtuoso-t +wait +foreground
